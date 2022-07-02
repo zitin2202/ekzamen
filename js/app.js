828472aab9133335e7a6303648be582c51a895c3
zitin2202 <zit2202@yandex.ru>
2022-07-02 10:08:41 +0300

    first_commit

---
+++ js/app.js
@@ -0,0 +1,211 @@
+products = [
+    {name:'Apple iPad',price:1000,img:'macbook-air.jpg'},
+    {name:'Apple iPad2',price:2000,img:'macbook-air.jpg'},
+    {name:'Apple iPad3',price:3000,img:'macbook-air.jpg'},
+    {name:'Apple iPad4',price:4000,img:'macbook-air.jpg'},
+    {name:'Apple iPad5',price:5000,img:'macbook-air.jpg'},
+    {name:'Apple iPad6',price:6000,img:'macbook-air.jpg'},
+    {name:'Apple iPad7',price:7000,img:'macbook-air.jpg'},
+    {name:'Apple iPad8',price:8000,img:'macbook-air.jpg'},
+]
+
+alert(products[3]['img']);
+
+
+
+function CreateTable(){
+    let table = document.createElement('table')
+    body.appendChild(table)
+    table.classList.add("table")
+    table.classList.add("table-striped")
+    table.innerHTML=table.innerHTML+
+        `        <div  class="catalog-rows">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="thumb-wrapper">
+                        <div class="img-box">
+                            <img src="/image/macbook-air.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="thumb-content">
+                            <h4>Apple iPad</h4>
+
+                            <p class="item-price"><b>$369.00</b></p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+`
+
+    return table
+}
+function CreateTbody(massiv,table)
+{
+    let row = document.createElement('tbody')
+    table.appendChild(tbody)
+
+    massiv.map((i)=>tbody.innerHTML=
+        tbody.innerHTML+
+        `<tr>
+    <td>${i.hotel}</td>
+    <td>${i.city}</td>
+    <td>${i.rating}</td>
+    <td>${i.price}</td>
+    <td>${i.time.getHours()} : ${i.time.getMinutes()}</td>
+    </tr>`
+
+    )
+}
---
+++ js/scroll.js
@@ -0,0 +1,24 @@
+window.addEventListener('scroll',e=>{
+
+    let navbarObj = document.getElementById('navbar')
+    let navbarClasses = navbarObj.classList
+    let style = window.getComputedStyle(navbarObj)
+    let height = style.getPropertyValue('height')
+    console.log(parseInt(height))
+    active_class = "menu_scrolled"
+
+    if(pageYOffset>parseInt(height)) navbarClasses.add(active_class)
+    else navbarClasses.remove(active_class)
+
+    // let active_class = 'navbar_scrolled'
+    // if(pageXOffset>400){
+    //
+    // }
+})
+
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
